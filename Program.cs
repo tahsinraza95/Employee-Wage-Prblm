5b99744c742789691c8cdcca22f584dd20f21122
tahsinraza95 <tahsinr693@gmail.com>
2023-01-31 17:22:25 +0530

    Added: Employee Wage Problem

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Employee_Wage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int RANDOM = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 1);
+            if (empCheck == RANDOM)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
